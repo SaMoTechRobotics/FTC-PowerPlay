a03a91ef3d8c5ea2bbb4bf7a2c631f0bc7608d13
AwesomeMarley <bgkirbyson@gmail.com>
2022-09-28 18:56:10 -0700

    Init

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/TestOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/TestOp.java
@@ -12,13 +12,7 @@ public class TestOp extends LinearOpMode {
         
 
         while (opModeIsActive()) {
-            if (TestStorage.SecureYay == "Yay!") {
-                telemetry.addData("Updated Saved Data", "");
-                TestStorage.SecureYay = "Let's go!";
-            }
-
             telemetry.addData("ROBOT IS WORKING?", "Yes it is!");
-            telemetry.addData("Saved data", TestStorage.SecureYay);
             telemetry.update();
         }
     }
