219ae4571b97e81f66ab9e3f56c0882c871f7d44
AwesomeMarley <bgkirbyson@gmail.com>
2022-10-01 20:26:21 -0700

    For launching on PLTW comp

---
+++ OpenCode.bat
@@ -0,0 +1,5 @@
+@cd D:\Java\bin
+@set PATH=D:\Java\bin;%PATH%
+@set PATH=C:\Users\fusion\AppData\Local\Programs\Microsoft VS Code;%PATH%
+@set PATH=C:\Users\fusion\Documents\Error-Logs\Git\Git\cmd;%PATH%
+code .
