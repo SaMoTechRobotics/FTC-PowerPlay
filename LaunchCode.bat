ae1a40d3435a2953720f981c4b5d4d7af477fd1f
AwesomeMarley <bgkirbyson@gmail.com>
2022-10-02 07:13:30 -0700

    Done

--- LaunchCode.bat
+++
@@ -1,6 +0,0 @@
-@cd C:\Users\fusion\Documents\Error-Logs\Code\FTC-PowerPlay
-@set PATH=C:\Users\fusion\Documents\Error-Logs\Assets\Java\bin;%PATH%
-@set PATH=C:\Users\fusion\AppData\Local\Programs\Microsoft VS Code;%PATH%
-@set PATH=C:\Users\fusion\Documents\Error-Logs\Assets\Git\Git\cmd\git.exe;%PATH%
-@set PATH=C:\Users\fusion\Documents\Error-Logs\Assets\Android\platform-tools;%PATH%
-code .
