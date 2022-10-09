160269047a71db664e559da50d9f8bec0662801c
AwesomeMarley <bgkirbyson@gmail.com>
2022-10-09 14:06:00 -0700

    Made slide functional

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/TeamCode/Drive/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/TeamCode/Drive/Drive.java
@@ -7,30 +7,44 @@ import com.acmerobotics.dashboard.FtcDashboard;
 @TeleOp(name = "Drive", group = "TeleOp")
 public class Drive extends LinearOpMode {
 
-
     private Slide Slide;
     private Chassis Chassis;
-    
+
     @Override
     public void runOpMode() throws InterruptedException {
-        
-        FtcDashboard dashboard = FtcDashboard.getInstance();
-        
-        Slide = new Slide(
-            hardwareMap.get(DcMotor.class, "slide")
-        );
 
-        Chassis = new Chassis(
-            hardwareMap.get(DcMotor.class, "frontLeft"),
-            hardwareMap.get(DcMotor.class, "frontRight"),
-            hardwareMap.get(DcMotor.class, "backLeft"),
-            hardwareMap.get(DcMotor.class, "backRight")
-        );
-       
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+
+        // Initialize the slide
+        Slide = new Slide(hardwareMap.get(DcMotor.class, "slide"));
+
+        // Chassis = new Chassis(
+        // hardwareMap.get(DcMotor.class, "frontLeft"),
+        // hardwareMap.get(DcMotor.class, "frontRight"),
+        // hardwareMap.get(DcMotor.class, "backLeft"),
+        // hardwareMap.get(DcMotor.class, "backRight")
+        // );
+
         waitForStart();
-        
+
         while (opModeIsActive()) {
-           
+
+            if (gamepad1.dpad_up) {
+                Slide.setHeight(SlideHeight.HighPole, SlideSpeed.Max);
+            }
+
+            if (gamepad1.dpad_down) {
+                Slide.setHeight(SlideHeight.LowPole, SlideSpeed.Max);
+            }
+
+            if (gamepad1.dpad_left) {
+                Slide.setHeight(SlideHeight.MidPole, SlideSpeed.Max);
+            }
+
+            if (gamepad1.dpad_right) {
+                Slide.setHeight(SlideHeight.Ground, SlideSpeed.Max);
+            }
+
         }
     }
 }
