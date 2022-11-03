cf8c09a9261c5a84de1ecdca549909b2fc75151e
AwesomeMarley <bgkirbyson@gmail.com>
2022-11-02 20:28:52 -0700

    Everything functional

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/TeamCode/Assets/Classes/Slide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/TeamCode/Assets/Classes/Slide.java
@@ -1,68 +1,107 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.acmerobotics.dashboard.config.Config;
-
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 /**
  * Slide class which contains all the methods for the slide of the robot
-*/
+ */
 public class Slide {
-    private DcMotor SlideMotor;
 
-    /**
-     * Creates a new slide with only 1 motor
-     * @param SlideMotor The motor that will be used for the slide as both left and right
-    */     
-    public Slide(DcMotor SlideMotor) {
-        this.SlideMotor = SlideMotor;
+  private DcMotor SlideMotor;
+
+  private static double Speed = SlideSpeed.Max;
+
+  /**
+   * Creates a new slide with only 1 motor
+   *
+   * @param SlideMotor The motor that will be used for the slide as both left and
+   *                   right
+   */
+  public Slide(DcMotor SlideMotor) {
+    this.SlideMotor = SlideMotor;
+  }
+
+  /**
+   * Sets the power of motors
+   *
+   * @param power The power to set the motors to, from -1.0 to 1.0
+   */
+  public final void setPower(double power) {
+    this.SlideMotor.setPower(power);
+  }
+
+  /**
+   * Sets the mode of the motors
+   *
+   * @param mode The mode to set the motors to, from DcMotor.RunMode enum
+   */
+  public final void setMode(DcMotor.RunMode mode) {
+    this.SlideMotor.setMode(mode);
+  }
+
+  /**
+   * Sets the target position of the motors
+   *
+   * @param target The target position in ticks
+   */
+  public final void setTarget(int target) {
+    this.SlideMotor.setTargetPosition(target);
+  }
+
+  /**
+   * Sets the target position of the motors
+   *
+   * @param height The target position as percentage
+   */
+  public final void setHeight(double height, double speed) {
+    int ticks = this.percentToTicks(height);
+
+    this.setTarget(ticks);
+    this.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+    this.setPower(speed);
+
+    this.setPower(SlideSpeed.Stop);
+    this.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+  }
+
+  /**
+   * Updates the slide with controls
+   * @param power The power to set the motors to, from -1.0 to 1.0 from the gamepad2 left stick y
+   * @param up The up button on the dpad
+   * @param left The left button on the dpad
+   * @param down The down button on the dpad
+   * @param right The right button on the dpad
+   */
+  public final void updateWithControls(
+    double power,
+    boolean up,
+    boolean left,
+    boolean down,
+    boolean right
+  ) {
+    if (up) {
+      this.setHeight(SlideHeight.HighPole, this.Speed); // Slide set to high pole height if dpad up is pressed
+    } else if (left) {
+      this.setHeight(SlideHeight.MidPole, this.Speed); // Slide set to mid pole height if dpad left is pressed
+    } else if (down) {
+      this.setHeight(SlideHeight.LowPole, this.Speed); // Slide set to low pole height if dpad down is pressed
+    } else if (right) {
+      this.setHeight(SlideHeight.Ground, this.Speed); // Slide set to ground height if dpad right is pressed
+    } else if (power != 0) {
+      this.setPower(power); // Slide set to power if left stick y is not 0
+    } else {
+      this.setPower(SlideSpeed.Stop); // Slide set to 0 if no buttons are pressed and left stick y is 0
     }
+  }
 
-    /**
-     * Sets the power of motors
-     * @param power The power to set the motors to, from -1.0 to 1.0
-    */
-    public void setPower(double power) {
-        this.SlideMotor.setPower(power);
-    }
-
-    /**
-     * Sets the mode of the motors
-     * @param mode The mode to set the motors to, from DcMotor.RunMode enum
-    */
-    public void setMode(DcMotor.RunMode mode) {
-        this.SlideMotor.setMode(mode);
-    }
-
-    /**
-     * Sets the target position of the motors
-     * @param target The target position in ticks
-    */
-    public void setTarget(int target) {
-        this.SlideMotor.setTargetPosition(target);
-    }
-   
-    /**
-     * Sets the target position of the motors
-     * @param height The target position as percentage
-    */
-    public void setHeight(double height, double speed) {
-        int ticks = this.percentToTicks(height);
-
-        this.setTarget(ticks);
-        this.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-        this.setPower(speed);
-
-        this.setPower(SlideSpeed.Stop);
-        this.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-    }
-
-    /**
-     * Converts a percentage to ticks
-     * @param height The target position as percentage
-    */
-    private int percentToTicks(double percent) {
-        return (int)(percent / 100 * SlideHeight.MaxTicks);
-    }
+  /**
+   * Converts a percentage to ticks
+   *
+   * @param height The target position as percentage
+   */
+  private final int percentToTicks(double percent) {
+    return (int) (percent / 100 * SlideHeight.MaxTicks);
+  }
 }
