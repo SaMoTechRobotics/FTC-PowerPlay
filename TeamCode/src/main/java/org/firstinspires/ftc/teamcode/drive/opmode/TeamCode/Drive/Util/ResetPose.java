625970fb1a94c7c926d4ec3fc8e5da72e099546b
AwesomeMarley <bgkirbyson@gmail.com>
2022-11-17 13:31:19 -0800

    Added ResetPose opmode to reset the robots position

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/TeamCode/Drive/Util/ResetPose.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.drive.opmode.TeamCode.Drive.Util;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.drive.opmode.TeamCode.Assets.Constants.Chassis.PoseStorage;
+
+@TeleOp(name = "Reset Pose", group = "Util")
+public class ResetPose extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        telemetry.addData("Status", "Ready to reset pose");
+        telemetry.addData("Current Pose", PoseStorage.currentPose);
+        telemetry.update();
+
+        waitForStart();
+        if (isStopRequested()) return;
+
+        PoseStorage.reset();
+
+        telemetry.addData("Status", "Reset pose complete");
+        telemetry.addData("Current Pose", PoseStorage.currentPose);
+        telemetry.update();
+    }
+}
