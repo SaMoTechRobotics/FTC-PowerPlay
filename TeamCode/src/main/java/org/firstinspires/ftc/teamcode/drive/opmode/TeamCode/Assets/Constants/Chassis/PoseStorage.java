b9a6a1b9db79f6289984f6ac3d28482f2250e99f
AwesomeMarley <bgkirbyson@gmail.com>
2022-11-22 09:08:02 -0800

    Added starting positions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/TeamCode/Assets/Constants/Chassis/PoseStorage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/TeamCode/Assets/Constants/Chassis/PoseStorage.java
@@ -6,7 +6,20 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
  * PoseStorage class which stores the pose of the robot between opmodes
  */
 public class PoseStorage {
+    /**
+     * Current pose of the robot, saved between opmodes
+     */
     public static Pose2d currentPose = new Pose2d();
+
+    /**
+     * Starting pose on the left
+     */
+    public static Pose2d leftStartPose = new Pose2d(-60, -48, Math.toRadians(90));
+
+    /**
+     * Starting pose on the right
+     */
+    public static Pose2d rightStartPose = new Pose2d(-60, -48, Math.toRadians(90));
 
     public static void reset() {
         currentPose = new Pose2d();
