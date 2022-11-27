5b71b60198262d3cd2ed6fa324ada8927a98c77f
AwesomeMarley <bgkirbyson@gmail.com>
2022-11-27 09:26:23 -0800

    Reconfigured claw positions for new claw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/TeamCode/Assets/Constants/Claw/ClawPosition.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/TeamCode/Assets/Constants/Claw/ClawPosition.java
@@ -5,24 +5,24 @@ import com.acmerobotics.dashboard.config.Config;
 @Config
 public class ClawPosition {
 
-  //open: 40, close: 90
-  /**
-   * Max and min positions of the claw, ex: 0.0 to 1.0 resulting in 1.0 range
-   */
-  public static double Max = 1.0;
-  public static double Min = 0.0;
+    //open: 40, close: 90
+    /**
+     * Max and min positions of the claw, ex: 0.0 to 1.0 resulting in 1.0 range
+     */
+    public static double Max = 1.0;
+    public static double Min = 0.0;
 
-  /**
-   * The max open and close positions of the claw
-   */
-  public static double MaxOpen = 100;
-  public static double MaxClosed = 0;
+    /**
+     * The max open and close positions of the claw
+     */
+    public static double MaxOpen = 100;
+    public static double MaxClosed = 0;
 
-  /**
-   * The open and close positions of the claw
-   */
-  public static double Open = 35;
-  public static double Close = 60;
+    /**
+     * The open and close positions of the claw
+     */
+    public static double Open = 30;
+    public static double Close = 15;
 
-  public static double ConeDistance = 3.0;
+    public static double ConeDistance = 3.0;
 }
