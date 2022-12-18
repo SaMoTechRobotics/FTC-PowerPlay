f3240937566346eea3c780e7e43ab2217d16a358
AwesomeMarley <bgkirbyson@gmail.com>
2022-12-17 16:32:31 -0800

    Added ArmTest, Arm now has diff speed

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/TeamCode/Drive/Tests/ArmTest.java
@@ -0,0 +1,78 @@
+package org.firstinspires.ftc.teamcode.drive.opmode.TeamCode.Drive.Tests;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.arcrobotics.ftclib.gamepad.ToggleButtonReader;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.drive.opmode.TeamCode.Assets.Classes.Arm;
+import org.firstinspires.ftc.teamcode.drive.opmode.TeamCode.Assets.Classes.Claw;
+import org.firstinspires.ftc.teamcode.drive.opmode.TeamCode.Assets.Constants.Arm.ArmRotation;
+
+@Config
+@TeleOp(name = "ArmTest", group = "Tests")
+public class ArmTest extends LinearOpMode {
+
+    public static double ArmSpeed = 0.001;
+
+    public static double Delay = 0.0;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+
+        // Initialize the arm
+        org.firstinspires.ftc.teamcode.drive.opmode.TeamCode.Assets.Classes.Arm Arm = new Arm(hardwareMap.get(Servo.class, "arm"));
+        Arm.setRotation(ArmRotation.Center);
+
+        // Initialize the gamepad
+        GamepadEx Gamepad1 = new GamepadEx(gamepad1);
+        GamepadEx Gamepad2 = new GamepadEx(gamepad2);
+
+        double ArmTargetPos = ArmRotation.Center;
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            if(Gamepad2.wasJustPressed(GamepadKeys.Button.X)) {
+                ArmTargetPos = ArmRotation.Left;
+            } else if(Gamepad2.wasJustPressed(GamepadKeys.Button.B)) {
+                ArmTargetPos = ArmRotation.Right;
+            } else if(Gamepad2.wasJustPressed(GamepadKeys.Button.Y)) {
+                ArmTargetPos = ArmRotation.Center;
+            }
+
+
+            if(Arm.getRotation() < ArmTargetPos) {
+                Arm.setRotation(Arm.getRotation() + ArmSpeed);
+            } else if(Arm.getRotation() > ArmTargetPos) {
+                Arm.setRotation(Arm.getRotation() - ArmSpeed);
+            }
+
+            telemetry.addData("Arm Rotation", Arm.getRotation());
+            telemetry.addData("Arm Target Pos", ArmTargetPos);
+            telemetry.update();
+
+            if(Delay > 0) {
+                sleep((long) Delay);
+            }
+            Gamepad2.readButtons();
+        }
+    }
+}
+//
+////            arm.updateWithControls(
+////                    Gamepad2.getRightX(),
+////                    gamepad2.x,
+////                    gamepad2.b,
+////                    gamepad2.y
+////            );
+//        }
+//    }
+//}
