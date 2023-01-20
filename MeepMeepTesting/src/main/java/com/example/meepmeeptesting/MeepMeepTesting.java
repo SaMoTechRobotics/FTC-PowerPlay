5e9629673e15b15cf336b1d81d39e641ce9b6005
AwesomeMarley <bgkirbyson@gmail.com>
2023-01-20 09:03:42 -0800

    Made a traj sequence

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,8 +1,8 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
-import com.noahbres.meepmeep.core.colorscheme.ColorScheme;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
@@ -10,18 +10,38 @@ public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
+//        RoadRunnerBotEntity bot = new DefaultBotBuilder(meepMeep)
+//                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+//                .setConstraints(55.5, 30, Math.toRadians(50), Math.toRadians(180), 13.63)
+//                .setStartPose(new Pose2d(36, -63, Math.toRadians(90)))
+//                .setDimensions(13, 15)
+//                .followTrajectorySequence(drive ->
+//                        drive.trajectorySequenceBuilder(new Pose2d(36, -64, Math.toRadians(90)))
+//                                .forward(40)
+//                                .addDisplacementMarker(22, () -> {
+//                                    //Reading signal sleeve
+//                                })
+//                                .lineToLinearHeading(new Pose2d(30, -12, Math.toRadians(0)))
+//                                .build()
+//                );
+
         RoadRunnerBotEntity bot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(55.5, 30, Math.toRadians(50), Math.toRadians(180), 13.63)
-                .setStartPose(new Pose2d(36, -63, Math.toRadians(90)))
+                .setConstraints(55.5, 30, Math.toRadians(90), Math.toRadians(180), 13.63)
+//                .setStartPose(new Pose2d(64, -63, Math.toRadians(90)))
                 .setDimensions(13, 15)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(36, -64, Math.toRadians(90)))
-                                .forward(40)
-                                .addDisplacementMarker(22, () -> {
-                                    //Reading signal sleeve
-                                })
-                                .lineToLinearHeading(new Pose2d(30, -12, Math.toRadians(0)))
+                        drive.trajectorySequenceBuilder(new Pose2d(64, -63, Math.toRadians(90)))
+                                .strafeTo(new Vector2d(60, -60))
+                                .forward(35)
+                                .splineToLinearHeading(new Pose2d(48, -12, Math.toRadians(180)), Math.toRadians(180))
+                                .forward(96)
+                                .splineToLinearHeading(new Pose2d(-60, -25, Math.toRadians(270)), Math.toRadians(270))
+                                .forward(25)
+                                .splineToLinearHeading(new Pose2d(-48, -60, Math.toRadians(0)), Math.toRadians(0))
+                                .forward(86)
+                                .lineToLinearHeading(new Pose2d(60, -60, Math.toRadians(90)))
+                                .strafeTo(new Vector2d(64, -63))
                                 .build()
                 );
 
