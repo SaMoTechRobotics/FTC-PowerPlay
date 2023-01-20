be8d15b99e8b21daea8f5da3ad114f890fa10836
AwesomeMarley <bgkirbyson@gmail.com>
2023-01-20 09:07:10 -0800

    Added file to store old trajectories

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/OldTrajectories.java
@@ -0,0 +1,28 @@
+/**
+ * Circles around the field
+ * <p>
+ * Trajectories
+ */
+//RoadRunnerBotEntity bot = new DefaultBotBuilder(meepMeep)
+//        // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+//        .setConstraints(55.5, 30, Math.toRadians(90), Math.toRadians(180), 13.63)
+////                .setStartPose(new Pose2d(64, -63, Math.toRadians(90)))
+//        .setDimensions(13, 15)
+//        .followTrajectorySequence(drive ->
+//        drive.trajectorySequenceBuilder(new Pose2d(64, -63, Math.toRadians(90)))
+//        .strafeTo(new Vector2d(60, -60))
+//        .forward(35)
+//        .splineToLinearHeading(new Pose2d(48, -12, Math.toRadians(180)), Math.toRadians(180))
+//        .forward(96)
+//        .splineToLinearHeading(new Pose2d(-60, -25, Math.toRadians(270)), Math.toRadians(270))
+//        .forward(25)
+//        .splineToLinearHeading(new Pose2d(-48, -60, Math.toRadians(0)), Math.toRadians(0))
+//        .forward(86)
+//        .lineToLinearHeading(new Pose2d(60, -60, Math.toRadians(90)))
+//        .strafeTo(new Vector2d(64, -63))
+//        .build()
+//        );
+
+/**
+ * Trajectories
+ */
