7c523e557c7f4086a0f0516082fc9dfaca9c04be
AwesomeMarley <bgkirbyson@gmail.com>
2023-01-20 07:11:31 -0800

    Meep Meep Init

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -71,8 +71,8 @@ public class DriveConstants {
      * small and gradually increase them later after everything is working. All distance units are
      * inches.
      */
-    public static double MAX_VEL = 55.5;
-    public static double MAX_ACCEL = 30;
+    public static double MAX_VEL = 60;
+    public static double MAX_ACCEL = 40;
     public static double MAX_ANG_VEL = Math.toRadians(50); //30
     public static double MAX_ANG_ACCEL = Math.toRadians(180);
 
