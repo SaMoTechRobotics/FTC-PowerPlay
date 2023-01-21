13a51e65bcbfbe5a79ce512edb2faf432e09c73d
AwesomeMarley <bgkirbyson@gmail.com>
2023-01-20 22:13:09 -0800

    Updated test auto for 1/4 field

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/TeamCode/Auto/Fun/AutoNavField.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/TeamCode/Auto/Fun/AutoNavField.java
@@ -5,19 +5,19 @@ import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.drive.DriveConstants;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.drive.opmode.TeamCode.Assets.Constants.PodLift.PodLiftPosition;
 
 @Config
 @Autonomous(name = "AutoNavField", group = "Fun")
-@Disabled
 public class AutoNavField extends LinearOpMode {
 
-    public static double AccelSpeed = 100;
+    public static double AccelSpeed = 80;
     public static double Speed = 100;
     public static double TurnSpeed = 90;
 
@@ -29,7 +29,10 @@ public class AutoNavField extends LinearOpMode {
 
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
-        Pose2d startPose = new Pose2d(64, -63, Math.toRadians(0));
+        Servo podLift = hardwareMap.get(Servo.class, "podLift");
+        podLift.setPosition(PodLiftPosition.Down);
+
+        Pose2d startPose = new Pose2d(64, -63, Math.toRadians(90));
 
         drive.setPoseEstimate(startPose);
 
@@ -51,8 +54,11 @@ public class AutoNavField extends LinearOpMode {
                                 SampleMecanumDrive.getVelocityConstraint(Speed, TurnSpeed, DriveConstants.TRACK_WIDTH),
                                 SampleMecanumDrive.getAccelerationConstraint(AccelSpeed)
                         )
-                        .forward(96)
-                        .splineToLinearHeading(new Pose2d(-60, -25, Math.toRadians(270)), Math.toRadians(270),
+                        .forward(24,
+                                SampleMecanumDrive.getVelocityConstraint(Speed, TurnSpeed, DriveConstants.TRACK_WIDTH),
+                                SampleMecanumDrive.getAccelerationConstraint(AccelSpeed)
+                        )
+                        .splineToLinearHeading(new Pose2d(10, -25, Math.toRadians(270)), Math.toRadians(270),
                                 SampleMecanumDrive.getVelocityConstraint(Speed, TurnSpeed, DriveConstants.TRACK_WIDTH),
                                 SampleMecanumDrive.getAccelerationConstraint(AccelSpeed)
                         )
@@ -60,22 +66,19 @@ public class AutoNavField extends LinearOpMode {
                                 SampleMecanumDrive.getVelocityConstraint(Speed, TurnSpeed, DriveConstants.TRACK_WIDTH),
                                 SampleMecanumDrive.getAccelerationConstraint(AccelSpeed)
                         )
-                        .splineToLinearHeading(new Pose2d(-48, -60, Math.toRadians(0)), Math.toRadians(0),
+                        .splineToLinearHeading(new Pose2d(24, -60, Math.toRadians(0)), Math.toRadians(0),
                                 SampleMecanumDrive.getVelocityConstraint(Speed, TurnSpeed, DriveConstants.TRACK_WIDTH),
                                 SampleMecanumDrive.getAccelerationConstraint(AccelSpeed)
                         )
-                        .forward(86,
+                        .forward(10,
                                 SampleMecanumDrive.getVelocityConstraint(Speed, TurnSpeed, DriveConstants.TRACK_WIDTH),
                                 SampleMecanumDrive.getAccelerationConstraint(AccelSpeed)
                         )
-                        .lineToLinearHeading(new Pose2d(60, -60, Math.toRadians(90)),
+                        .lineToLinearHeading(new Pose2d(50, -60, Math.toRadians(90)),
                                 SampleMecanumDrive.getVelocityConstraint(Speed, TurnSpeed, DriveConstants.TRACK_WIDTH),
                                 SampleMecanumDrive.getAccelerationConstraint(AccelSpeed)
                         )
-                        .strafeTo(new Vector2d(64, -63),
-                                SampleMecanumDrive.getVelocityConstraint(Speed, TurnSpeed, DriveConstants.TRACK_WIDTH),
-                                SampleMecanumDrive.getAccelerationConstraint(AccelSpeed)
-                        )
+                        .strafeTo(new Vector2d(64, -63))
                         .build()
         );
 
