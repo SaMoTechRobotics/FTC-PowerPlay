cdec9d902f379af89da0e1735f67c047c7411238
AwesomeMarley <bgkirbyson@gmail.com>
2023-01-22 15:38:12 -0800

    Fixed starting rotation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/TeamCode/Auto/Constants/AutoPositions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/TeamCode/Auto/Constants/AutoPositions.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode.drive.opmode.TeamCode.Auto.Constants;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 
 public class AutoPositions {
-    public static Pose2d StartRight = new Pose2d(36, -63, Math.toRadians(0));
-    public static Pose2d StartLeft = new Pose2d(-36, -63, Math.toRadians(0));
+    public static Pose2d StartRight = new Pose2d(36, -63, Math.toRadians(270));
+    public static Pose2d StartLeft = new Pose2d(-36, -63, Math.toRadians(270));
 
 }
