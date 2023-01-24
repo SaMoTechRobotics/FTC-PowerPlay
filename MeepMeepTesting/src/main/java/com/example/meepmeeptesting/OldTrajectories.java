3e64c0c804b53de9dfbe68b7cb4681b637df064a
AwesomeMarley <bgkirbyson@gmail.com>
2023-01-24 14:22:29 -0800

    Saved AutoRightPro traj test

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/OldTrajectories.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/OldTrajectories.java
@@ -2,6 +2,10 @@
  * Circles around the field
  * <p>
  * Trajectories
+ * <p>
+ * Trajectories
+ * <p>
+ * Trajectories
  */
 //RoadRunnerBotEntity bot = new DefaultBotBuilder(meepMeep)
 //        // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
@@ -22,6 +26,65 @@
 //        .strafeTo(new Vector2d(64, -63))
 //        .build()
 //        );
+
+/*
+ * AutoRightPro
+ */
+/*
+
+        Pose2d startPose = new Pose2d(36, -63, Math.toRadians(270));
+
+
+        RoadRunnerBotEntity bot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(90, 75, Math.toRadians(90), Math.toRadians(180), 13.63)
+                .setDimensions(13, 15)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(startPose)
+                                .back(35)
+                                        .addDisplacementMarker(22, () -> {
+                                        //Reading signal sleeve
+                                        })
+                                        .waitSeconds(0.5)
+                                        .lineToLinearHeading(new Pose2d(34, -25, Math.toRadians(270)))
+                                        .waitSeconds(0.8)
+                                        .lineToLinearHeading(new Pose2d(40, -12, Math.toRadians(0)))
+                                        .waitSeconds(0.5)
+                                        .lineToLinearHeading(new Pose2d(56, -12, Math.toRadians(0)))
+                                        .waitSeconds(0.5) //start of 2 cone
+                                        .forward(7.5)
+                                        .waitSeconds(0.5)
+                                        .lineToLinearHeading(new Pose2d(5, -14, Math.toRadians(0)))
+                                        .waitSeconds(0.5)
+                                        .back(5) //auto align
+                                        .waitSeconds(0.8)
+                                        .strafeLeft(3)
+                                        .waitSeconds(0.5)
+                                        .lineToLinearHeading(new Pose2d(56, -12, Math.toRadians(0))) //end of 2 cone
+                                        .waitSeconds(0.5) //start of 3 cone
+                                        .forward(7.5)
+                                        .waitSeconds(0.5)
+                                        .lineToLinearHeading(new Pose2d(5, -14, Math.toRadians(0)))
+                                        .waitSeconds(0.5)
+                                        .back(5) //auto align
+                                        .waitSeconds(0.8)
+                                        .strafeLeft(3)
+                                        .waitSeconds(0.5)
+                                        .lineToLinearHeading(new Pose2d(56, -12, Math.toRadians(0))) //end of 3 cone
+                                        .waitSeconds(0.5) //start of 4 cone
+                                        .forward(7.5)
+                                        .waitSeconds(0.5)
+                                        .lineToLinearHeading(new Pose2d(30, -10, Math.toRadians(0)))
+                                        .waitSeconds(0.5)
+                                        .back(5) //auto align
+                                        .waitSeconds(0.8)
+                                        .strafeRight(3)
+                                        .waitSeconds(0.5)
+                                        .lineToLinearHeading(new Pose2d(56, -12, Math.toRadians(0))) //end of 4 cone
+                                        .build()
+                                        );
+
+*/
 
 /**
  * Trajectories
