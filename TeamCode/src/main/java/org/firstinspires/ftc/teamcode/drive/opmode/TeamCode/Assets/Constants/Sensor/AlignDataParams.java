a877f84cdd69390b314265985968804a84c9bd76
AwesomeMarley <bgkirbyson@gmail.com>
2023-02-22 19:26:34 -0800

    Changed Outlier Margin

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/TeamCode/Assets/Constants/Sensor/AlignDataParams.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/TeamCode/Assets/Constants/Sensor/AlignDataParams.java
@@ -1,7 +1,7 @@
 package org.firstinspires.ftc.teamcode.drive.opmode.TeamCode.Assets.Constants.Sensor;
 
 public class AlignDataParams {
-    public static double OutlierMargin = 2; //the margin of error to remove an outlier
+    public static double OutlierMargin = 1.5; //the margin of error to remove an outlier
     public static int MinimumDataAmount = 3; //how long the robot has to be losing the pole to check if it is still losing the pole
 
     public static double LosingPoleMargin = 1; //if the robot is losing the pole check
