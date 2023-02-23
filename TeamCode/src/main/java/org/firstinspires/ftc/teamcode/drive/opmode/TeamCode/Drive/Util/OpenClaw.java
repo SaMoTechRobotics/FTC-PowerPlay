9f6d03b1883c91635bed9ecfa9d488e6f0097446
AwesomeMarley <bgkirbyson@gmail.com>
2023-02-22 19:26:53 -0800

    Made util opmode to open/close claw

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/TeamCode/Drive/Util/OpenClaw.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode.drive.opmode.TeamCode.Drive.Util;
+
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.drive.opmode.TeamCode.Assets.Classes.Claw;
+import org.firstinspires.ftc.teamcode.drive.opmode.TeamCode.Assets.Constants.Claw.ClawPosition;
+
+@TeleOp(name = "Open Claw", group = "Drive")
+public class OpenClaw extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        Claw Claw =
+                new Claw(
+                        hardwareMap.get(Servo.class, "claw"),
+                        hardwareMap.get(DistanceSensor.class, "clawDistanceSensor"),
+                        hardwareMap.get(Servo.class, "poleBrace")
+                );
+        Claw.setOpenAmount(ClawPosition.PickupOpen);
+        Claw.open();
+
+        GamepadEx Gamepad2 = new GamepadEx(gamepad2);
+
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+        
+        Claw.close();
+
+        while (opModeIsActive()) {
+
+            if (Gamepad2.wasJustPressed(GamepadKeys.Button.LEFT_BUMPER)) {
+                Claw.open();
+            } else if (Gamepad2.wasJustPressed(GamepadKeys.Button.RIGHT_BUMPER)) {
+                Claw.close();
+            }
+            Gamepad2.readButtons();
+
+        }
+    }
+}
