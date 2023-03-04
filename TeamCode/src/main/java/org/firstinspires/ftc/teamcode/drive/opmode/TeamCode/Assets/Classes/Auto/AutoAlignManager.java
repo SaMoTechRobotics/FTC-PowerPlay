298d086c1563c607298c2e0285e477ac1d5cde3b
AwesomeMarley <bgkirbyson@gmail.com>
2023-03-04 14:45:51 -0800

    Setup auto center

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/TeamCode/Assets/Classes/Auto/AutoAlignManager.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/TeamCode/Assets/Classes/Auto/AutoAlignManager.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.drive.opmode.TeamCode.Assets.Classes.Auto
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.util.RobotLog;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
@@ -60,6 +61,70 @@ public class AutoAlignManager {
                     bestAlignPos.Position.getHeading() //keeps the same heading
             );
         }
+
+        public static Pose2d getCalculatedCenterPosition(AlignPos bestAlignPos, Chassis.PoleAlign alignStrafe, Pose2d currentPose) {
+            double PlaceDistance = alignStrafe == Chassis.PoleAlign.Left ? SensorDistances.LeftCenterDistance : SensorDistances.RightCenterDistance; //gets the place distance depending on aligning direction
+
+            //calculates the distance to align to the pole, positive means driving to left, negative means driving to right
+            double dist = alignStrafe == Chassis.PoleAlign.Left ? (bestAlignPos.SensorDistance - PlaceDistance) : -(bestAlignPos.SensorDistance - PlaceDistance);
+
+            double rotationalOffset = alignStrafe == Chassis.PoleAlign.Left ? 90 : -90; //gets the rotational offset depending on aligning direction
+            double perpendicularHeadingSin = Math.sin(bestAlignPos.Position.getHeading() + Math.toRadians(rotationalOffset)); //gets the sin of the heading with the offset
+            double perpendicularHeadingCos = Math.cos(bestAlignPos.Position.getHeading() + Math.toRadians(rotationalOffset)); //gets the cos of the heading with the offset
+
+            double offset = Math.abs(bestAlignPos.Position.getX() - currentPose.getX()); //gets the offset depending on aligning direction
+
+            double headingSin = Math.sin(bestAlignPos.Position.getHeading()); //gets the sin of the heading
+            double headingCos = Math.cos(bestAlignPos.Position.getHeading()); //gets the cos of the heading
+
+            return new Pose2d( //calculates the position to align to
+                    bestAlignPos.Position.getX() + (dist * perpendicularHeadingCos) + (offset * headingCos), //calculates the x value
+                    bestAlignPos.Position.getY() + (dist * perpendicularHeadingSin) + (offset * headingSin), //calculates the y value
+                    bestAlignPos.Position.getHeading() //keeps the same heading
+            );
+        }
+//
+//        public static Pose2d getCalculatedCenterPosition(AlignPos bestAlignPos, Chassis.PoleAlign alignStrafe) {
+//            double PlaceDistance = alignStrafe == Chassis.PoleAlign.Left ? SensorDistances.LeftCenterDistance : SensorDistances.RightCenterDistance;
+//
+//            double dist = alignStrafe == Chassis.PoleAlign.Left ? (bestAlignPos.SensorDistance - PlaceDistance) : -(bestAlignPos.SensorDistance - PlaceDistance);
+//
+//            double offset = alignStrafe == Chassis.PoleAlign.Left ? AlignDataParams.LeftForwardOffset : AlignDataParams.RightForwardOffset;
+//
+//            // Define the current point
+//            Vector2d currentPoint = new Vector2d(bestAlignPos.Position.getX(), bestAlignPos.Position.getY());
+//
+//            // Define the pivot point as a Pose2d object
+//            Pose2d centerPoint = new Pose2d(bestAlignPos.SensorDistance, 0, 0);
+//
+//            // Calculate the distance between the current point and the pivot point
+//            double distance = currentPoint.minus(centerPoint.vec()).norm();
+//
+//            // Translate the current point relative to the pivot point
+//            Vector2d translatedPoint = currentPoint.minus(centerPoint.vec());
+//
+//            // Calculate the normalized vector
+//            double mag = translatedPoint.norm();
+//            Vector2d normalizedPoint = new Vector2d(translatedPoint.getX() / mag, translatedPoint.getY() / mag);
+//
+//            // Multiply the normalized vector by the desired distance
+//            Vector2d scaledPoint = normalizedPoint.times(distance);
+//
+//            // Define the rotation angle in radians
+//            double rotationAngle = Math.toRadians(180); // 180 degrees clockwise
+//
+//            // Apply the rotation transformation
+//            Vector2d rotatedPoint = scaledPoint.rotated(rotationAngle);
+//
+//            // Translate the rotated point back to the original coordinate system
+//            Vector2d finalPoint = rotatedPoint.plus(centerPoint.vec());
+//
+//            return new Pose2d(
+//                    finalPoint.getX() + (dist * Math.cos(rotationAngle)) + (offset * Math.cos(0)),
+//                    finalPoint.getY() + (dist * Math.sin(rotationAngle)) + (offset * Math.sin(0)),
+//                    0
+//            );
+//        }
     }
 
     public static class AlignPos {
@@ -101,7 +166,7 @@ public class AutoAlignManager {
      * 5. Drive to the calculated distance
      * 6. Return true because it is aligned
      */
-    public static boolean smartAlign(SampleMecanumDrive drive, DistanceSensor leftSensor, DistanceSensor rightSensor, Chassis.PoleAlign alignDrive, Chassis.PoleAlign alignStrafe) {
+    public static boolean smartAlign(SampleMecanumDrive drive, DistanceSensor leftSensor, DistanceSensor rightSensor, Chassis.PoleAlign alignDrive, Chassis.PoleAlign alignStrafe, boolean... isCenter) {
         drive.update(); //updates the auto position
 
         double sensorDistance = alignStrafe == Chassis.PoleAlign.Left ? leftSensor.getDistance(DistanceUnit.INCH) : rightSensor.getDistance(DistanceUnit.INCH); //gets the distance from the sensor
@@ -116,7 +181,10 @@ public class AutoAlignManager {
                     SmartAlignData.getBestIndex(sortedAlignData) //gets the best align position
             ); //gets the best align position
 
-            smartAlignData.calculatedPosition = SmartAlignData.getCalculatedPosition(bestAlignPos, alignStrafe); //gets the calculated position
+            smartAlignData.calculatedPosition =
+                    (isCenter.length > 0 && isCenter[0]) ? //if isCenter is true
+                            SmartAlignData.getCalculatedCenterPosition(bestAlignPos, alignStrafe, drive.getPoseEstimate()) : //gets the calculated position to align with center of tile
+                            SmartAlignData.getCalculatedPosition(bestAlignPos, alignStrafe); //gets the calculated position to align with the pole
 
             drive.followTrajectory( // drive to calculated position
                     drive.trajectoryBuilder(drive.getPoseEstimate())
@@ -129,13 +197,15 @@ public class AutoAlignManager {
 //            this.smartAlignReset(); //reset smart align data for next time
             return true; //finished aligning
         } else if (smartAlignData.sawPole) {
-            boolean finishedDataCollection = false; //temp var to check if data collection is finished, starts as false
-            for (int i = 0; i < AlignDataParams.LosingPoleDataAmount; i++) { //checks if the sensor is losing the pole
-                if (smartAlignData.distances.size() > i + 1) { //if the distance list is long enough
-                    if (smartAlignData.distances.get(smartAlignData.distances.size() - 1 - i).SensorDistance < //if the current distance is less than the previous distance
-                            smartAlignData.distances.get(smartAlignData.distances.size() - 2 - i).SensorDistance + AlignDataParams.LosingPoleMargin) { //if the current distance is less than the previous distance plus the margin
-                        finishedDataCollection = true; //data collection is finished because the sensor is losing the pole
-                        break;
+            boolean finishedDataCollection = smartAlignData.distances.get(smartAlignData.distances.size() - 1).SensorDistance > SensorDistances.DetectAmount; //temp var to check if data collection is finished, starts as false
+            if (!finishedDataCollection) {
+                for (int i = 0; i < AlignDataParams.LosingPoleDataAmount; i++) { //checks if the sensor is losing the pole
+                    if (smartAlignData.distances.size() > i + 1) { //if the distance list is long enough
+                        if (smartAlignData.distances.get(smartAlignData.distances.size() - 1 - i).SensorDistance < //if the current distance is less than the previous distance
+                                smartAlignData.distances.get(smartAlignData.distances.size() - 2 - i).SensorDistance + AlignDataParams.LosingPoleMargin) { //if the current distance is less than the previous distance plus the margin
+                            finishedDataCollection = true; //data collection is finished because the sensor is losing the pole
+                            break;
+                        }
                     }
                 }
             }
@@ -150,6 +220,7 @@ public class AutoAlignManager {
                         )
                 );
                 smartAlignData.distances.add(new AlignPos(drive.getPoseEstimate(), sensorDistance));
+                RobotLog.d("SensorDistance: " + sensorDistance);
             } else { //stop moving if all data collected
                 drive.setWeightedDrivePower(
                         new Pose2d(
