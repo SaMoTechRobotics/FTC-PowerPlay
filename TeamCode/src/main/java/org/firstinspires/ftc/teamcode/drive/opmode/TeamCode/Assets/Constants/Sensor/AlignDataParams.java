6dded6d0a61c91d10abb1e4b3610b58ebd3224be
AwesomeMarley <bgkirbyson@gmail.com>
2023-03-07 10:12:47 -0800

    Tweaked for more accuracy and proper giveup

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/TeamCode/Assets/Constants/Sensor/AlignDataParams.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/TeamCode/Assets/Constants/Sensor/AlignDataParams.java
@@ -5,9 +5,9 @@ import com.acmerobotics.dashboard.config.Config;
 @Config
 public class AlignDataParams {
     public static double OutlierMargin = 1.5; //the margin of error to remove an outlier
-    public static int MinimumDataAmount = 3; //how long the robot has to be losing the pole to check if it is still losing the pole
+    public static int MinimumDataAmount = 8; //how long the robot has to be losing the pole to check if it is still losing the pole
 
-    public static double LosingPoleMargin = 1; //if the robot is losing the pole check
+    public static double LosingPoleMargin = 2; //if the robot is losing the pole check
     public static int LosingPoleDataAmount = 1; //how long (in terms of distances collected) the robot has to be losing the pole to check if it is still losing the pole
 
     public static double FindGiveUpTime = 5;
